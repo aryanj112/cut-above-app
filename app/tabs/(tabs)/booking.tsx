cef1f392d0bf74ca8acd10f337753a40025ac249
rbitar1 <rbitar1@terpmail.umd.edu>
2025-09-16 19:14:55 -0400

    Added logo and tabs

---
+++ app/tabs/(tabs)/booking.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function BookingPage() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Booking Page</Text>
+    </View>
+  );
+}
